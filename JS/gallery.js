50e37ef602f94bc83e881fbf17869c1913dcce54
TarikSanich <141185095+TarikSanich@users.noreply.github.com>
2024-01-11 22:43:46 +0200

    mod1

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,108 +1,116 @@
-const images = [ 
-    
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
-    description: "Hokkaido Flower",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-    description: "Container Haulage Freight",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-    description: "Aerial Beach View",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-    description: "Flower Blooms",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-    description: "Alpine Mountains",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-    description: "Mountain Lake Sailing",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    description: "Alpine Spring Meadows",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    description: "Nature Landscape",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    description: "Lighthouse Coast Sea",
-  },
+
+    'use strict';
+
+const images = [
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+		description: 'Hokkaido Flower',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+		description: 'Container Haulage Freight',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+		description: 'Aerial Beach View',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+		description: 'Flower Blooms',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+		description: 'Alpine Mountains',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+		description: 'Mountain Lake Sailing',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+		description: 'Alpine Spring Meadows',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+		description: 'Nature Landscape',
+	},
+	{
+		preview:
+			'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+		original:
+			'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+		description: 'Lighthouse Coast Sea',
+	},
 ];
-const gallery = document.querySelector('.gallery');
-const modal = document.querySelector('.modal');
-const modalImage = document.querySelector('.modal-image');
 
-// Функція для створення елементів галереї
-function createGallery() {
-  const galleryItems = images.map(image => {
-    const item = document.createElement('div');
-    item.classList.add('gallery-item');
-    
-    const img = document.createElement('img');
-    img.src = image.preview;
-    img.alt = image.description;
-    img.dataset.source = image.original;
-    img.classList.add('gallery-image');
-    
-    item.appendChild(img);
-    return item;
-  });
-  
-  gallery.append(...galleryItems);
-}
+const galleryList = document.querySelector('.gallery');
 
-// Функція для відкриття модального вікна
-function openModal(source, alt) {
-  modalImage.src = source;
-  modalImage.alt = alt;
-  modal.style.display = 'block';
-}
+const listItems = images
+	.map(
+		({ preview, original, description }) =>
+			`<li class='gallery-item'>
+					<a class='gallery-link' href='${original}'>
+  					<img
+    					class='gallery-image'
+    					src='${preview}'
+    					data-source='${original}'
+    					alt='${description}'
+  					/>
+					</a>
+				</li>`
+	)
+	.join('');
 
-// Додаємо обробник кліків на елементи галереї
-gallery.addEventListener('click', e => {
-  if (e.target.classList.contains('gallery-image')) {
-    const source = e.target.dataset.source;
-    const alt = e.target.alt;
-    openModal(source, alt);
-  }
+galleryList.innerHTML = listItems;
+
+galleryList.addEventListener('click', event => {
+	event.preventDefault();
+	if (event.target.nodeName !== 'IMG') return;
+	const clickedImage = basicLightbox.create(
+		`
+		<div class="modal">
+		<img src='${event.target.dataset.source}'>
+		</div>
+  `,
+		{
+			onShow: () => {
+				document.addEventListener('keydown', onKey);
+			},
+			onClose: () => {
+				document.removeEventListener('keydown', onKey);
+			},
+		}
+	);
+	clickedImage.show();
+
+	function onKey(params) {
+		if (params.code === 'Escape') {
+			clickedImage.close();
+			document.removeEventListener('keydown', onKey);
+		}
+	}
 });
-
-// Додаємо обробник кліку для закриття модального вікна
-modal.addEventListener('click', e => {
-  if (
